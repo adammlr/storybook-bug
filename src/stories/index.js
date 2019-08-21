e9eb4dcfed00ac5c3ebe628e1938e7d3470293a1
Adam Miller <adam.miller@csgi.com>
2019-08-21 12:50:45 -0500

    addon-info

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,9 +2,16 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withInfo } from "@storybook/addon-info";
 
 import Button from "../Button";
 
-storiesOf("Button", module).add("with text", () => (
-  <Button onClick={action("clicked")}>Hello Button</Button>
-));
+storiesOf("Button", module)
+  .addDecorator(withInfo)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ));
+
+storiesOf("div", module)
+  .addDecorator(withInfo)
+  .add("with text", () => <div>Hello div</div>);
